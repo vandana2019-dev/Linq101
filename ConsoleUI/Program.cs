7a3ddec6cdca6e37c844d5d6c60b5d356bd63816
vandana l <vandana.developer2019@gmail.com>
2022-04-20 22:29:23 +1200

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinqLibrary;
+
+Console.WriteLine("Hello, World!");
+List<Person> people = ListManager.LoadSampleData();
+
+//people = people.OrderBy(x => x.LastName).ToList();
+
+//people = people.OrderByDescending(x => x.LastName).ThenBy(x => x.YearsExperience).ToList();
+
+//people = people.Where(x => x.YearsExperience > 5).ToList();
+
+people = people.Where(x => x.YearsExperience > 5 && x.Birthday.Month == 3).ToList();
+
+
+int yearsTotal = 0;
+
+//yearsTotal = people.Sum(x => x.YearsExperience);
+
+yearsTotal = people.Where(x => x.Birthday.Month == 3).Sum(x => x.YearsExperience);
+
+Console.WriteLine($" The total years experience is {yearsTotal}");
+
+foreach (var person in people)
+{
+    Console.WriteLine($" { person.FirstName } { person.LastName } ({ person.Birthday.ToShortDateString()})");
+}
+
+Console.ReadLine();
---
+++ LinqLibrary/ListManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLibrary
+{
+    public class ListManager
+    {
+        public static List<Person> LoadSampleData()
+        {
+            List<Person> output = new List<Person>();
+
+            output.Add(new Person { FirstName = "Tim", LastName="Corey", Birthday = Convert.ToDateTime("02/10/1970"), YearsExperience = 4});
+            output.Add(new Person { FirstName = "Joe", LastName = "Smith", Birthday = Convert.ToDateTime("03/12/1970"), YearsExperience = 2 });
+            output.Add(new Person { FirstName = "Sue", LastName = "Storm", Birthday = Convert.ToDateTime("01/3/1970"), YearsExperience = 1 });
+            output.Add(new Person { FirstName = "Sara", LastName = "Jones", Birthday = Convert.ToDateTime("03/6/1970"), YearsExperience = 5 });
+            output.Add(new Person { FirstName = "Jamie", LastName = "Doe", Birthday = Convert.ToDateTime("02/5/1970"), YearsExperience = 8 });
+            output.Add(new Person { FirstName = "Mary", LastName = "Smith", Birthday = Convert.ToDateTime("01/11/1970"), YearsExperience = 2 });
+
+
+            return output;
+        }
+    }
+}
